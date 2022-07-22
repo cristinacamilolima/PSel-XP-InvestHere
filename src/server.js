265b3e027ac6e5f889201968780a22942b97f828
Cristina Camilo <cristinacamillo.lima@gmail.com>
2022-07-22 12:17:26 -0300

    chore: settings

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+
 const app = express();
-app.set('view-engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index.ejs', {nome: 'João'});
-})
-app.listen(3000);
+
+app.use(express.json());
+
+module.exports = app;
